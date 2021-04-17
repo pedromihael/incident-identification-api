d83859092411bb5149d30cd6ab5fa490a753fc55
Pedro Mihael <pedromvpaes@gmail.com>
2021-04-17 14:47:15 -0400

    chore: add base files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.disable('x-powered-by');
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log('Server is runnig on port 3000');
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is awake');
+});
