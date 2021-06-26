e6718d4b9c25ed9acae15776833d9aa36a674f5f
Pedro Mihael <pedromvpaes@gmail.com>
2021-06-26 19:00:39 -0400

    chore: change port to 3003

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ app.disable('x-powered-by');
 app.use(cors());
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is runnig on port 3000');
+app.listen(3003, () => {
+  console.log('Server is runnig on port 3003');
 });
 
 app.use(incidentRouter);
