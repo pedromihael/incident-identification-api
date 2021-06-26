022f3a0f45656607d6cadfe18d4a89ee7e691c34
Pedro Mihael <pedromvpaes@gmail.com>
2021-06-25 22:41:18 -0400

    chore: update incident controller

--- src/domains/incident/controllers/index.js
+++ src/domains/incident/controllers/index.js
@@ -1,29 +1,61 @@
 const knex = require('../../../db');
 
+const providersController = require('../../provider/controllers');
+const projectsController = require('../../project/controllers');
+
+const ApiErrorFactory = require('../../../shared/factories/ApiErrorFactory');
+const errorFactory = new ApiErrorFactory();
+
 const getAllIncidents = async () => {
-  const results = await knex('incident');
-  return results;
+  try {
+    const results = await knex('incident');
+    return results;
+  } catch (error) {
+    return errorFactory.createError(error, 'getAllIncidents');
+  }
 };
 
 const getIncidentById = async (id) => {
-  const result = await knex('incident').where({ id });
-
-  return result;
+  try {
+    const result = await knex('incident').where({ id });
+    return result;
+  } catch (error) {
+    return errorFactory.createError(error, 'getIncidentById');
+  }
 };
 
-const registerIncident = async (description, fk_severity, fk_project) => {
-  const result = await knex('incident').insert({ description, fk_severity, fk_project });
+const getIncidentsByProject = async (fk_project) => {
+  try {
+    const result = await knex('incident').where({ fk_project });
+    return result;
+  } catch (error) {
+    return errorFactory.createError(error, 'getIncidentsByProject');
+  }
+};
 
-  return result;
+const registerIncident = async (description, fk_severity, fk_project, fk_provider) => {
+  try {
+    const result = await knex('incident').insert({ description, fk_severity, fk_project });
+    // TODO: mudar confiabilidade
+
+    const newProjectReliability = null; // pegar do model
+    const newProviderReliability = null; // pegar do model
+
+    await projectsController.updateProvider(fk_project, 'reliability_percentage', newProjectReliability);
+    await providersController.updateProvider(fk_provider, 'reliability_percentage', newProviderReliability);
+
+    return result;
+  } catch (error) {
+    return errorFactory.createError(error, 'registerIncident');
+  }
 };
 
 const updateIncident = async (id, description) => {
   try {
     const result = await knex('incident').update({ description }).where({ id });
     return { result };
-  } catch (err) {
-    console.log('err', err);
-    return err;
+  } catch (error) {
+    return errorFactory.createError(error, 'updateIncident');
   }
 };
 
@@ -31,10 +63,16 @@ const deleteIncident = async (id) => {
   try {
     const result = await knex('incident').where({ id }).delete();
     return { result };
-  } catch (err) {
-    console.log('err', err);
-    return err;
+  } catch (error) {
+    return errorFactory.createError(error, 'deleteIncident');
   }
 };
 
-module.exports = { getAllIncidents, getIncidentById, registerIncident, updateIncident, deleteIncident };
+module.exports = {
+  getAllIncidents,
+  getIncidentById,
+  getIncidentsByProject,
+  registerIncident,
+  updateIncident,
+  deleteIncident,
+};
