c65b66a8cacd2dbed5fb9a6c354c07df418c15a3
Pedro Mihael <pedromvpaes@gmail.com>
2021-06-29 13:15:37 -0400

    ci: envs to db

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,36 @@
 // Update with your config settings.
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
   development: {
     client: 'pg',
     connection: {
-      username: 'postgres',
-      password: 'postgres',
-      // port: 5432,
-      database: 'incidents_tool',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/db/migrations`,
+    },
+    seeds: {
+      tableName: 'knex_seeds',
+      directory: `${__dirname}/src/db/seeds`,
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
     },
     pool: {
       min: 2,
