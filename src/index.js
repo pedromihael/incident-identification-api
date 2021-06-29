d0135b1c6197f28c3962bbd6e45765429b05ac74
Pedro Mihael <pedromvpaes@gmail.com>
2021-06-29 13:19:53 -0400

    ci: add env port

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
 const incidentRouter = require('./domains/incident/routes');
@@ -5,14 +6,17 @@ const projectRouter = require('./domains/project/routes');
 const providerRouter = require('./domains/provider/routes');
 const severityRouter = require('./domains/severity/routes');
 const reliabilityRouter = require('./domains/reliability/routes');
+
+dotenv.config();
+
 const app = express();
 
 app.use(cors());
 app.disable('x-powered-by');
 app.use(express.json());
 
-app.listen(3003, () => {
-  console.log('Server is runnig on port 3003');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is runnig on port ${process.env.PORT}`);
 });
 
 app.use(incidentRouter);
